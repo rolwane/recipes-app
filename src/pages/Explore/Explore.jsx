f3fb8315410befb8f16661e9889381661126b812
Istvan <olivaistvan@gmail.com>
2022-03-09 16:25:06 -0300

    Requisitos 67~69

--- src/pages/Explore/Explore.jsx
+++ src/pages/Explore/Explore.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 
@@ -6,6 +7,12 @@ function Explore() {
   return (
     <section>
       <Header title="Explore" />
+      <Link to="/explore/foods">
+        <button type="button" data-testid="explore-foods">Explore Foods</button>
+      </Link>
+      <Link to="/explore/drinks">
+        <button type="button" data-testid="explore-drinks">Explore Drinks</button>
+      </Link>
       <Footer />
     </section>
 
