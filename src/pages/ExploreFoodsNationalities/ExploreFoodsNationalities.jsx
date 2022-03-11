44c3a83e09ebc041ec5a356560df2935db5ad223
Istvan <olivaistvan@gmail.com>
2022-03-11 17:33:35 -0300

    Incompleto ainda

--- src/pages/ExploreFoodsNationalities/ExploreFoodsNationalities.jsx
+++ src/pages/ExploreFoodsNationalities/ExploreFoodsNationalities.jsx
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from 'react';
-import { getAreasFoods, getFoodsByArea } from '../../services/foodsAPI';
+import React, { useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { getAreasFoods, getFoodsByArea, getFoods } from '../../services/foodsAPI';
 import { MAX_FOODS_AND_DRINKS } from '../../helpers/constants';
 
 // import components
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import RecipeCard from '../../components/RepiceCard/RecipeCard';
+import RecipeContext from '../../context/RecipeContext';
 
 function FoodNationalities() {
+  const { foodsRecipe } = useContext(RecipeContext);
   const [nationalities, setNationalities] = useState([]);
-  const [selectedNationality, setselectedNationality] = useState('American');
-  const [foodNationality, setfoodNationality] = useState([]);
+  const [selectedNationality, setselectedNationality] = useState('');
+  const [foodNationality, setFoodNationality] = useState(foodsRecipe);
 
   useEffect(() => {
     getAreasFoods().then(({ meals }) => {
@@ -18,14 +21,21 @@ function FoodNationalities() {
       const filterNationalities = filterMap
         .filter((food, index) => filterMap.indexOf(food) === index);
       setNationalities(filterNationalities);
+      // setselectedNationality(filterNationalities[0]);
     });
   }, []);
 
   console.log(foodNationality);
 
   useEffect(() => {
-    getFoodsByArea(selectedNationality).then(({ meals }) => setfoodNationality(meals));
+    if (selectedNationality) {
+      getFoodsByArea(selectedNationality).then(({ meals }) => setFoodNationality(meals));
+    }
   }, [selectedNationality]);
+
+  useEffect(() => {
+    getFoods().then(({ meals }) => setFoodNationality(meals));
+  }, []);
 
   return (
     <section>
@@ -38,7 +48,7 @@ function FoodNationalities() {
 
           <option
             key={ item }
-            // data-testid={ `${item}-option` }
+            data-testid={ `${item}-option` }
             value={ item }
           >
             {item}
@@ -46,12 +56,18 @@ function FoodNationalities() {
       </select>
       <section>
         {foodNationality.map((food, index) => (index < MAX_FOODS_AND_DRINKS && (
-          <RecipeCard
-            key={ food.idMeal }
-            image={ food.strMealThumb }
-            name={ food.strMeal }
-            index={ food.index }
-          />
+          <Link
+            to={ `/foods/${food.idMeal}` }
+            // data-testid={ `${index}-recipe-card` }
+            key={ index }
+          >
+            <RecipeCard
+              key={ food.idMeal }
+              image={ food.strMealThumb }
+              name={ food.strMeal }
+              index={ index }
+            />
+          </Link>
         )))}
       </section>
       <Footer />
