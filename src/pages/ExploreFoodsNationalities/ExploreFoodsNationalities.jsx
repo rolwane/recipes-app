e2bf0a79d202a82a7553716aa80e1b325d21e488
Istvan <olivaistvan@gmail.com>
2022-03-11 15:07:45 -0300

    Tela de nacionalidades

--- src/pages/ExploreFoodsNationalities/ExploreFoodsNationalities.jsx
+++ src/pages/ExploreFoodsNationalities/ExploreFoodsNationalities.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
+import Input from '../../components/Input/Input';
+import RecipeContext from '../../context/RecipeContext';
 
 function FoodNationalities() {
+  const { foodsRecipe } = useContext(RecipeContext);
+
+  const nationalities = foodsRecipe.map(({ strArea }) => strArea);
+  const filterNationalities = nationalities
+    .filter((food, index) => nationalities.indexOf(food) === index);
+
+  // const nationalitiesOptions = filterNationalities
+  //   .map((item, index) => (<option key={ index }>{ item }</option>));
+
   return (
     <section>
       <Header title="Explore Nationalities" renderSearch />
+      <Input
+        type="select"
+        testId="explore-by-nationality-dropdown"
+        value={ filterNationalities }
+      />
+      {console.log(filterNationalities)}
       <Footer />
     </section>
   );
 }
 
 export default FoodNationalities;
+
+// Referência : https://pt.stackoverflow.com/questions/16483/remover-elementos-repetido-dentro-de-um-array-em-javascript;
